2bc2b01c857c634b438d41bb7dd36ead4dcf75a6
josue rojas <josuerojas.rojas@gmail.com>
2018-04-04 12:28:25 -0400

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Unknown-Timers
+A simple react-express website showing random timers submitted by others in a 'simple' design
+
+### Somethings used
+- react
+- express
+- postgres sql
+- [this tutorial](http://ericsowell.com/blog/2017/5/16/create-react-app-and-express)
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../timer-ui/build')));
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../timer-ui/build', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+});
